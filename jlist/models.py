96ebce327d3f54af8c50921b9824148a06c88ccb
Adrienne Dreyfus <adrienne.dreyfus@tufts.edu>
2013-02-23 15:32:31 -0500

    added databases

--- jlist/models.py
+++ jlist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+
+class UserProfile(models.model):
+    user = models.ForeignKey(User, unique=True, related_name="profile")
+    items = models.ManyToManyField(Item, related_name="profile")
+
+
+
+class Item(models.model):
+    id = models.AutoField(primary_key=True)
+    seller = models.ForeignKey(UserProfile, unique=False, related_name="item" )
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 1000)
+    price = models.DecimalField()
+    sold = models.BooleanField()
+    date_added = models.DateField(auto_now_add=True)
+    photo = models.URLField(max_length=200)
+
+
+    
+
